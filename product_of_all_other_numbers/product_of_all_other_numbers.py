b839133c1b785e143ea8ed4171bcd9278c898dfb
Michel Laporte <michlaporte15@gmail.com>
2020-06-18 02:54:16 -0300

    module 3, mvp

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -3,9 +3,24 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def product_of_all_other_numbers(arr):
-    # Your code here
+        size = len(arr)
+        product = [0] * size
+        product[0] = 1
 
-    pass
+        for i in range(1, size):
+            product[i] = arr[i - 1] * product[i - 1]
+
+        R = 1
+
+        for i in reversed(range(size)):
+            product[i] = product[i] * R
+            R *= arr[i]
+        
+        return product  
+    
+        index = range(0, len(arr))
+        
+
 
 
 if __name__ == '__main__':
@@ -14,3 +29,14 @@ if __name__ == '__main__':
     arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
 
     print(f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
+
+
+
+
+
+# def multiply(numbers):  
+#     total = 1
+#     for x in numbers:
+#         total *= x  
+#     return total  
+# print(multiply((8, 2, 3, -1, 7)))
--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,9 +3,14 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
+    single = []
+    for i in arr:
+        if i not in single:
+            single.append(i)
+        else:
+            single.remove(i)
 
-    pass
+    return int(single[0])
 
 
 if __name__ == '__main__':
